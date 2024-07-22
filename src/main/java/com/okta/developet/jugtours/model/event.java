a056cabc5f4f1326794375a93b81bc44f4229bc2
hasharas <hasharamsankalpam@gmail.com>
2024-07-23 01:12:06 +0530

    create event java class file

--- src/main/java/com/okta/developet/jugtours/model/event.java
+++ src/main/java/com/okta/developet/jugtours/model/event.java
@@ -1,5 +1,31 @@
-package com.okta.developet.jugtours.model;
+package com.okta.developer.jugtours.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import java.time.Instant;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class event {
+
+      @Id
+      @GeneratedValue
+      private Long id;
+      private Instant date;
+      private String title;
+      private String description;
+      @ManyToMany
+      private Set<User> attendees;
 
 }
